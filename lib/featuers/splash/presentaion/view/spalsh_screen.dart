3f2f3f14352fe4dc4fa2bbc82d180191bf86707f
coba <coba2003x@gmail.com>
2023-07-21 07:58:04 +0300

    splash screen done

---
+++ lib/featuers/splash/presentaion/view/spalsh_screen.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/featuers/splash/presentaion/view/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
---
+++ lib/featuers/splash/presentaion/view/widgets/splash_view_body.dart
@@ -0,0 +1,53 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(
+          AssetsData.logo,
+          height: 300,
+        ),
+        SlideTransition(
+          position: slideAnimation,
+          child: const Text(
+            textAlign: TextAlign.center,
+            "Read Free books",
+          ),
+        ),
+      ],
+    );
+  }
+}
